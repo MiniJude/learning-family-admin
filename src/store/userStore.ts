2fb1ce0900d484030aa790ef4c71e69c2f582a57
minijude <914753389@qq.com>
2025-01-22 00:36:14 +0800

    chore: get menu permission from server

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -5,8 +5,9 @@ import { createJSONStorage, persist } from "zustand/middleware";
 
 import userService, { type SignInReq } from "@/api/services/userService";
 
+import { PERMISSION_LIST } from "@/_mock/assets";
 import { toast } from "sonner";
-import type { UserInfo, UserToken } from "#/entity";
+import type { Permission, UserInfo, UserToken } from "#/entity";
 import { StorageEnum } from "#/enum";
 
 const { VITE_APP_HOMEPAGE: HOMEPAGE } = import.meta.env;
@@ -67,6 +68,7 @@ export const useSignIn = () => {
 		try {
 			const res = await signInMutation.mutateAsync(data);
 			const { user, accessToken, refreshToken } = res;
+			// user.permissions = PERMISSION_LIST as Permission[];
 			setUserToken({ accessToken, refreshToken });
 			setUserInfo(user);
 			navigatge(HOMEPAGE, { replace: true });
